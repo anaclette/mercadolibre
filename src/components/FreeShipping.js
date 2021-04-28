9be5c1f326a3a344034996488ef065b2cba1408f
anaclette <laclauana@gmail.com>
2021-04-28 16:02:28 -0300

    added truck img on items with free shipping

---
+++ src/components/FreeShipping.js
@@ -0,0 +1,9 @@
+const FreeShipping = ({ logo }) => {
+	return (
+		<div className="FreeShipping">
+			<img alt="Logo de Envío Gratis" src={logo} />
+		</div>
+	);
+};
+
+export default FreeShipping;
--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,8 +1,13 @@
+import FreeShipping from './FreeShipping';
+import FreeShippingLogo from './Free-shipping.png';
+import './FreeShipping.scss';
+
 const Card = ({ product, handleClickInfo }) => {
 	return (
 		<div className="Card">
 			<div>
 				<img alt={product.title} src={product.thumbnail} />
+				{product.shipping.free_shipping === 'true' && <FreeShipping logo={FreeShippingLogo} />}
 			</div>
 			<h2>{product.title}</h2>
 			<p>${product.price}</p>
